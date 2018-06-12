a80a7ea317baae61a2f2c5d25875d0fec649d082
ContentsViewer <fivetwothreesix@gmail.com>
2018-06-12 22:05:57 +0900

    getch追加

--- test.py
+++ test.py
@@ -2,13 +2,11 @@
 
 # import os
 
-
-
-
 import rospy
 
 from dynamixel_servomotor_controller import *
 from xl_config import *
+from getch import Getch
 
 
 #from time import sleep
@@ -35,8 +33,8 @@ def main():
     motor_controller.current_id = DXL_ID
 
 
-    motor_controller.set_operating_mode(XLConfig.OPERATING_MODE_POSITION_CONTROL_MODE)
-    # motor_controller.set_operating_mode(XLConfig.OPERATING_MODE_VELOCITY_CONTROL_MODE)
+    #motor_controller.set_operating_mode(XLConfig.OPERATING_MODE_POSITION_CONTROL_MODE)
+    motor_controller.set_operating_mode(XLConfig.OPERATING_MODE_VELOCITY_CONTROL_MODE)
 
     print "torque_enable: "
     motor_controller.set_torque_enable(1)
@@ -47,45 +45,67 @@ def main():
     
 
 
-    speed = 0.0
-    dir = 0.2
-    max_speed = 0.5
+    # speed = 0.0
+    # dir = 0.2
+    # max_speed = 0.5
 
 
 
-    sw = False
+    # sw = False
+
+    # getch = Getch()
 
     while not rospy.is_shutdown():
-        sw ^= True
+        # sw ^= True
 
-        motor_controller.set_led(int(sw))
+        # motor_controller.set_led(int(sw))
 
-        motor_controller.set_goal_position(0 if sw else 180)
+        # motor_controller.set_goal_position(0 if sw else 180)
         # motor_controller.set_goal_position(0.0)
 
 
-        print "present_position: " + str(motor_controller.present_position())
+        #print "present_position: " + str(motor_controller.present_position())
 
+        # print getch()
+        # continue
 
+        print "cmd << ",
+        cmd = raw_input()
 
+        isAccepted = True
+        if(cmd == "exit"):
+            print "program is now to end..."
+            break
+        else: 
+            try:
+                speed = float(cmd)
+                print "speed: " + str(speed)
+                # motor_controller.set_goal_velocity(speed)
+            except:
+                isAccepted = False
+        
+        
+        if not isAccepted:
+            print "[Error] command parse error!"
 
+        print "OK"
 
-        print "loop"
+        # print "loop"
         
         # motor_controller.set_goal_velocity(speed)
         # print "max_position: " +  str(motor_controller.max_position_limit())
         # print "min_position: " +  str(motor_controller.min_position_limit())
 
-        speed += dir
-        if speed > max_speed:
-            speed = max_speed
-            dir *= -1
+        # speed += dir
+        # if speed > max_speed:
+        #     speed = max_speed
+        #     dir *= -1
 
-        if speed < -max_speed:
-            speed = -max_speed
-            dir *= -1
+        # if speed < -max_speed:
+        #     speed = -max_speed
+        #     dir *= -1
 
-        print str(speed)
+        # print str(speed)
 
         timer.sleep()
 
